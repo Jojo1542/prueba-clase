dc0ecf557d4bd404ffaaa3359f74a39352b820e2
Jojo1542 <Jojo1542@WIN-P906EEQBS6J.prox1.jojo1542.es>
2023-01-21 18:01:08 +0100

    chore: initial commit:

---
+++ src/Prueba.java
@@ -0,0 +1,33 @@
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		int numero = 10;
+		System.out.println(numero);
+		numero = numero / 10;
+		System.out.println(numero);
+		
+		new Thread(new Runnable() {
+				
+				@Override
+				public void run() {
+					int numero2 = 10;
+					System.out.println(numero2);
+					try {
+						numero2 = numero2 / 0;
+						System.out.println(numero2);
+					} catch(Exception ex) {
+						ex.printStackTrace();
+					}
+					
+					System.out.println(numero2);
+					System.out.println(numero2);
+					System.out.println(numero2);
+				}
+				
+				
+		}).run();
+		
+	}
+
+}
